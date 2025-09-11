c7d034ff7a3bb5399ae598cb3eff430d102f2427
Sébastien Lallement <sebastien.lallement@etu.unice.fr>
2025-09-11 10:09:00 +0200

    Rock and stone joker added (art + desc) no logic yet

--- Balatro-Beignet-Mod/main.lua
+++ Balatro-Beignet-Mod/main.lua
@@ -475,6 +475,51 @@ SMODS.Joker{
     end
 }
 
+-------------------- ROCK AND STONE --------------------------
+
+SMODS.Atlas{
+    key = 'RockAndStone', --atlas key
+    path = 'drg.png', --atlas' path in (yourMod)/assets/1x or (yourMod)/assets/2x
+    px = 71, --width of one card
+    py = 95 -- height of one card
+}
+SMODS.Joker{
+    key = 'RockAndStone', --joker key
+    loc_txt = { -- local text
+        name = 'Rock And Stone',
+        text = {
+          'If played hand contains,',
+          'a {C:chips}stone card{}, {C:green}1 in 5{} chance to',
+          'destroy it and gain a random thing',
+          '{C:inactive}(playing card, tarot card, joker,',
+          '{C:inactive}money, hand, discard, tag)'
+        },
+    },
+    atlas = 'RockAndStone', --atlas' key
+    rarity = 3, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 7, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = true, --can it be eternal
+    perishable_compat = true, --can it be perishable
+    pos = {x = 0, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    config = { 
+      extra = {
+        Xmult = 1 --configurable value
+      }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {center.ability.extra.Xmult}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+    calculate = function(self,card,context)
+
+        -- No logic yet
+
+    end
+}
+
 ----------------------------------------------
 ------------MOD CODE END----------------------
     
